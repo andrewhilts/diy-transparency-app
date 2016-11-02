33a634b5e3e830c560c6c39832aebe7c02eb0988
Andrew Hilts <andrewchilts@gmail.com>
2016-11-02 16:16:45 -0400

    Windows install using virtualenv

--- windows-diy-transparency-run.bat
+++ windows-diy-transparency-run.bat
@@ -1,5 +1,3 @@
-SET dir=%cd%
-echo %dir%
 echo:
 echo "Running DIY Transparency application server. Please keep this window open, and wait for your browser to open to http://localhost:5000
-C:\Python27\python.exe %dir%\app.py
+venv\Scripts\activate & python app.py
--- windows-install-requirements.bat
+++ windows-install-requirements.bat
@@ -1,1 +1,4 @@
-C:\Python27\Scripts\pip.exe install -r requirements.txt
+C:\Python27\Scripts\pip.exe install virtualenv
+C:\Python27\Scripts\virtualenv venv
+venv\Scripts\activate
+pip install -r requirements.txt
