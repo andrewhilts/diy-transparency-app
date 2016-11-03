76e608f2b6553d431b13dc495d3fadf98074ee49
Andrew Hilts <andrewchilts@gmail.com>
2016-11-03 10:46:34 -0400

    Mac install without sudo?

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if ! type "virtualenv" > /dev/null; then
-	curl -L -o virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
-	python virtualenv.py -p /usr/bin/python2.7 venv
-else
-	virtualenv -p /usr/bin/python2.7 venv
+	pip install --user virtualenv
+	if [ "$(uname)" == "Darwin" ]; then
+		echo 'export PATH=/Users/$USER/Library/Python/2.7/bin:$PATH' >> ~/.bash_profile
+		source ~/.bash_profile
+	fi
 fi
+virtualenv -p /usr/bin/python2.7 venv
 source venv/bin/activate
 pip install -r requirements.txt
 exit
