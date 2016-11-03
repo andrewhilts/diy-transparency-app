2c9d2414f82aab1f5a6ebef1e807e1c0260ad8ce
Andrew Hilts <andrewchilts@gmail.com>
2016-11-03 10:13:13 -0400

    add sudoless virtualenv to install.sh if not available

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-pip install virtualenv
-virtualenv -p /usr/bin/python2.7 venv
+if ! type "virtualenv" > /dev/null; then
+	curl -L -o virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
+	python virtualenv.py -p /usr/bin/python2.7 venv
+else
+	virtualenv -p /usr/bin/python2.7 venv
+fi
 source venv/bin/activate
 pip install -r requirements.txt
 exit
