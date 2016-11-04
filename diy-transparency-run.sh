a01f8491480c269573a2bbd69c0881263ea1def3
Andrew Hilts <andrewchilts@gmail.com>
2016-11-04 10:32:15 -0400

    Update diy-transparency-run.sh

--- diy-transparency-run.sh
+++ diy-transparency-run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-SCRIPTPATH=${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}
-DIRECTORY=$(dirname "${SCRIPTPATH}")
-cd "${DIRECTORY}"
 source venv/bin/activate
 printf "\n\n\n\n\nLaunching DIY Transparency application server. Please leave this window open throughout your use of the app.\n\n\n\n\n"
 python app.py
